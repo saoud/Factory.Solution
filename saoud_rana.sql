301e6601fdb4d564c71b76d52dd3bc88757c6361
Saoud <saoud@saoud.dev>
2021-06-16 09:03:03 -0700

    adding in working sql database

--- saoud_rana.sql
+++ saoud_rana.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
 --
--- Host: localhost    Database: factory_solution_2
+-- Host: localhost    Database: factory_solution_3
 -- ------------------------------------------------------
 -- Server version	8.0.15
 
@@ -16,66 +16,43 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `__EFMigrationsHistory`
+-- Dumping data for table `__EFMigrationsHistory`
 --
 
-DROP TABLE IF EXISTS `__EFMigrationsHistory`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `__EFMigrationsHistory` (
-  `MigrationId` varchar(95) NOT NULL,
-  `ProductVersion` varchar(32) NOT NULL,
-  PRIMARY KEY (`MigrationId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `__EFMigrationsHistory` WRITE;
+/*!40000 ALTER TABLE `__EFMigrationsHistory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `__EFMigrationsHistory` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `Engineers`
+-- Dumping data for table `Engineers`
 --
 
-DROP TABLE IF EXISTS `Engineers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `Engineers` (
-  `EngineerId` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `DateHired` datetime(6) NOT NULL,
-  PRIMARY KEY (`EngineerId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `Engineers` WRITE;
+/*!40000 ALTER TABLE `Engineers` DISABLE KEYS */;
+INSERT INTO `Engineers` VALUES (1,'Saoud');
+/*!40000 ALTER TABLE `Engineers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `MachineEngineers`
+-- Dumping data for table `MachineEngineer`
 --
 
-DROP TABLE IF EXISTS `MachineEngineers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `MachineEngineers` (
-  `MachineEngineerId` int(11) NOT NULL AUTO_INCREMENT,
-  `EngineerId` int(11) NOT NULL,
-  `MachineId` int(11) NOT NULL,
-  PRIMARY KEY (`MachineEngineerId`),
-  UNIQUE KEY `IX_MachineEngineers_EngineerId_MachineId` (`EngineerId`,`MachineId`),
-  KEY `IX_MachineEngineers_MachineId` (`MachineId`),
-  CONSTRAINT `FK_MachineEngineers_Engineers_EngineerId` FOREIGN KEY (`EngineerId`) REFERENCES `engineers` (`EngineerId`) ON DELETE CASCADE,
-  CONSTRAINT `FK_MachineEngineers_Machines_MachineId` FOREIGN KEY (`MachineId`) REFERENCES `machines` (`MachineId`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `MachineEngineer` WRITE;
+/*!40000 ALTER TABLE `MachineEngineer` DISABLE KEYS */;
+INSERT INTO `MachineEngineer` VALUES (1,1,1);
+/*!40000 ALTER TABLE `MachineEngineer` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `Machines`
+-- Dumping data for table `Machines`
 --
 
-DROP TABLE IF EXISTS `Machines`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `Machines` (
-  `MachineId` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  PRIMARY KEY (`MachineId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `Machines` WRITE;
+/*!40000 ALTER TABLE `Machines` DISABLE KEYS */;
+INSERT INTO `Machines` VALUES (1,'Auger');
+/*!40000 ALTER TABLE `Machines` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -86,4 +63,4 @@ CREATE TABLE `Machines` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-05 22:47:53
+-- Dump completed on 2021-06-16  9:02:41
